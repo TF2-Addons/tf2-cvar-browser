3c670bfb1555f561ed1144d2dab5b2183b679a7d
Techdoodle <chris@techchrism.me>
2020-12-27 18:52:47 -0800

    properly initialize localforage prefix and vuetify theme

--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
-import vuetify from './plugins/vuetify';
+import Vuetify from 'vuetify/lib';
+import localforage from 'localforage';
+
+localforage.config({
+    name: 'tf2-cvar-browser'
+});
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
-new Vue({
-    store,
-    vuetify,
-    render: function(h)
+(async() =>
+{
+    let theme = await localforage.getItem('theme');
+    if(!theme)
     {
-        return h(App);
+        theme = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'dark' : 'light';
     }
-}).$mount('#app');
+    
+    new Vue({
+        vuetify: new Vuetify({
+            theme: {
+                dark: theme !== 'light'
+            }
+        }),
+        store,
+        render: function(h)
+        {
+            return h(App);
+        }
+    }).$mount('#app');
+})();
